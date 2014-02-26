68a411feb50f30b9546294353c9271fb66554106
Wei Wang <tskatom@gmail.com>
2014-02-25 22:36:20 -0500

    add copy

---
+++ predict.sh
@@ -0,0 +1,3 @@
+
+n icews_dpc.py --icews --create --date "2014-02-12" --out "./output" --file "2014-02-12_icews.json" --surr 'auditrail_data_all_Feb12.csv'
+
---
+++ upload.sh
@@ -0,0 +1,4 @@
+warn=$1
+surr=$2
+python /home/weiwang/workspace/embers/prototype/s3_put.py -p incoming/predictions/civilunrest/icews files $1
+python /home/weiwang/workspace/embers/prototype/s3_put.py -p surrogates/civilunrest/icews files $2
