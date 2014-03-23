5542461fba2cc07e846cbb7b701e36b3acd64d0c
Wei Wang <tskatom@gmail.com>
2014-03-23 13:18:37 -0400

    update

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 warn=$1
 surr=$2
-python /home/vic/work/scratch/prototype/s3_put.py -p incoming/predictions/civilunrest/icews files $1
-python /home/vic/work/scratch/prototype/s3_put.py -p surrogates/civilunrest/icews files $2
+python /home/weiwang/workspace/scratch/prototype/s3_put.py -p incoming/predictions/civilunrest/icews files $1
+python /home/weiwang/workspace/scratch/prototype/s3_put.py -p surrogates/civilunrest/icews files $2
